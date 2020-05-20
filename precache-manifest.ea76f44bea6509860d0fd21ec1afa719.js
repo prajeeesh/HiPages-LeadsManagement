d198a1c0eb8458fb07c9f9a6d15da1a4cac552c2
PRAJEESH KOTTAYAM KANDY EZHUTHANI <prajeesh.ezhuthani@outlook.com>
2020-05-20 10:52:47 +1000

    Updates

---
+++ precache-manifest.ea76f44bea6509860d0fd21ec1afa719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab26f026f17072be23de054447bc04c",
+    "url": "/prajeeesh/HiPages-LeadsManagement/index.html"
+  },
+  {
+    "revision": "2c0cc8440edafae005ce",
+    "url": "/prajeeesh/HiPages-LeadsManagement/static/css/main.7ede8e52.chunk.css"
+  },
+  {
+    "revision": "d272c737d43f602cef51",
+    "url": "/prajeeesh/HiPages-LeadsManagement/static/js/2.821e73b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/prajeeesh/HiPages-LeadsManagement/static/js/2.821e73b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c0cc8440edafae005ce",
+    "url": "/prajeeesh/HiPages-LeadsManagement/static/js/main.b0071414.chunk.js"
+  },
+  {
+    "revision": "ba8311853d05661749b2",
+    "url": "/prajeeesh/HiPages-LeadsManagement/static/js/runtime-main.e65d5129.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prajeeesh/HiPages-LeadsManagement/static/media/logo.5d5d9eef.svg"
+  }
+]);
